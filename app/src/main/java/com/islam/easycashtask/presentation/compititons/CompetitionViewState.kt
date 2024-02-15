1220d4d8950fe5cff7f6acf68fcf63559d8f8137
mostafaowies <mostafaowies@yahoo.com>
2024-02-15 12:39:17 +0200

    check end work api

--- app/src/main/java/com/islam/easycashtask/presentation/compititons/CompetitionViewState.kt
+++ app/src/main/java/com/islam/easycashtask/presentation/compititons/CompetitionViewState.kt
@@ -1,7 +1,6 @@
 package com.islam.easycashtask.presentation.compititons
 
 import com.islam.easycashtask.model.competition.Competition
-import com.islam.easycashtask.model.competition.EndWorkData
 import com.islam.easycashtask.model.competition.WorkState
 import com.islam.easycashtask.model.competition.WorkStateData
 
